d043ecd2727d3566ba28657e42d04786f0d3c4d1
yp_zheng <yupeng.zheng@tongji.edu.cn>
2022-01-01 16:19:48 +0800

    [delete]

--- src/main/java/greedy/algorithm/ReconstructQueue.java
+++ src/main/java/greedy/algorithm/ReconstructQueue.java
@@ -6,35 +6,6 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ReconstructQueue {
-    //    public static int[][] reconstructQueue(int[][] people) {
-//        if (people == null || people.length <= 1) {
-//            return people;
-//        }
-//        List<int[]> res = new ArrayList<>();
-//        Arrays.sort(people, Comparator.comparingInt(i -> i[1]));
-//        for (int[] man : people) {
-//            int rank = man[1];
-//            if (res.size() == 0) {
-//                res.add(man);
-//                continue;
-//            }
-//            for (int[] sortMan : res) {
-//                if (rank == 0) {
-//                    if (sortMan[0] > man[0]) {
-//                        res.add(res.indexOf(sortMan), man);
-//                    } else {
-//                        res.add(res.indexOf(sortMan)+1, man);
-//                    }
-//                    break;
-//                }
-//                if (sortMan[0] >= man[0]) {
-//                    rank --;
-//                }
-//            }
-//        }
-//        return res.toArray(new int[res.size()][]);
-//    }
-//
     public static int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people, (i, j) -> {
             if (i[1] != j[1]) {
